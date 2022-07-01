37e925f4883e4a15b79c89ff6a05001d172d48d2
Masumkhan984 <masumkhan9984@gmail.com>
2022-07-01 22:47:04 +0600

    update

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import useProducts from '../../hooks/useProducts';
 
 const Inventory = () => {
+    const [products] = useProducts();
     return (
         <div>
-            <h1>This is inventory page</h1>
+
+            <h1>This is inventory page{products.length}</h1>
         </div>
     );
 };
